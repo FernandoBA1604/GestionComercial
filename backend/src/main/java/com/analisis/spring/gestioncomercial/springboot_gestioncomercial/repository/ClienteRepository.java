9d7d0d3b65867e0d2e07f2a049e2b867d49123df
Andre Salas <167485672+iT0XHL@users.noreply.github.com>
2025-06-06 21:22:06 -0500

    Revert "Avance de la segunda parte del proyecto"

--- backend/src/main/java/com/analisis/spring/gestioncomercial/springboot_gestioncomercial/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.analisis.spring.gestioncomercial.springboot_gestioncomercial.repository;
-
-import com.analisis.spring.gestioncomercial.springboot_gestioncomercial.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
-    Cliente findByNroRuc(String nroRuc);
-}
